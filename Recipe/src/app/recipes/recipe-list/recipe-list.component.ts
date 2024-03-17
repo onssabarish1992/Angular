23137955d079be7ce571511d2d442374e342b29b
Sabarish Nair <121101278@umail.ucc.ie>
2024-03-17 15:42:25 +0000

    Project Recipes
    This is my first project for recipes and shopping list

---
+++ Recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes:Recipe[] = [
+    new Recipe('Test Recipe','Test Description','https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg')
+  ];
+
+}
